b938104abdc256c617284f9cb4c2cf54eb114d67
NickChew <118730370+NickChew@users.noreply.github.com>
2022-11-29 21:39:02 +0000

    new stuff

--- if-else-switch/IfelseSwitchAct8.js
+++ if-else-switch/IfelseSwitchAct8.js
@@ -8,15 +8,16 @@ vowels = ["a","e","i","o","u"];
 
 strLen = string.length;
     for (let i=0; i<strLen; i++){
-        for(j=0; j<6; j++)
-        if (string[i] == vowels[j]) {
+        for(j=0; j<6; j++){
+            if (string[i] == vowels[j]) {
             console.log (`${vowels[j]} "Vowel found at" ${[i]}`);
-        }  
-    }                                                                   // need to pass i out of loop
-    console.log (`${[i]}`)
+            }  
+        }                                                  // need to pass i and the vowel out of loop at end
+    }                                                      //following wont work  
+
     //console.log (`"string location" ${[i]} ${item.vowels[j]} "was last Vowel found"`);
        
-        
+     
     
-//console.log (`"string location" ${i} ${item.vowels[j]} "was last Vowel found"`);
+
 
