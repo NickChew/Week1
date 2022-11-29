9848e590eacf1ca870e61816dfe606e520bc4356
NickChew <118730370+NickChew@users.noreply.github.com>
2022-11-29 22:05:33 +0000

    new stuff

--- Arrays n Loops/Arrays&LoopsAct7.js
+++ Arrays n Loops/Arrays&LoopsAct7.js
@@ -1,11 +1,19 @@
-let bobsFollowers = ["Bob","Andy","Steve","Lyndsey"];
-let hannahsFollowers = ["Xara","Jane","Lyndsey","Megan"];
+// Activity 7:
+// Research on do...while loop, find 
+// out about the difference between 
+// for loop, while loop and do...while 
+// loop. Give an example of each. 
+// What are the pros and cons?
 
 
-for(let i=0;i<bobsFollowers.length;i++){
-    for(var j=0;j<hannahsFollowers.length;j++){
-        if(bobsFollowers[i]==hannahsFollowers[j]){
-            console.log(bobsFollowers[i],hannahsFollowers[j])
-        }
-    }
+for (let i = 0; i <= 20; i++) {     //for loops loops through the blocks of code specified number of times.
+   console.log("test loop");        // a Difinitive Loop
+}                                   //ouputs test loop 20 times
+
+
+var counter = 20;                   //While…loop is used to repeat the block of code until the condition is false.
+var j = 0;                          // Indefinite Loop runs while a condition is true until its false
+while (j < counter) {
+  console.log("test loop2");
+  j++
 }
