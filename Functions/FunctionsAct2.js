9a50f27e96ad364cb2e67782ae46ccadf2a0d1ef
NickChew <118730370+NickChew@users.noreply.github.com>
2022-11-30 16:17:19 +0000

    newest files

--- Functions/FunctionsAct2.js
+++ Functions/FunctionsAct2.js
@@ -2,12 +2,31 @@
 // Edit the below snippet to include two parameters and a
 // running order count updated when the function is called
 
+//let orderCount = 0;
+
+// //const takeOrder = (topping) => {
+//     console.log(`Pizza with ${topping}`);
+//     orderCount++;
+// }
+
+// takeOrder("pineapple");
+
+
 let orderCount = 0;
 
-const takeOrder = (topping) => {
-    console.log (`Pizza with ${topping}`);
-    orderCount++;
+const takeOrder = (topping1, topping2 ) => {                
+    console.log(`Pizza with ${topping1} and ${topping2} `);     // display pizza with the two toppings
+    orderCount++;                                               //count Plus 1
 }
 
-takeOrder("pineapple");
+takeOrder("Pineapple", "Ham");                              //1st order
+console.log(`There are currently ${orderCount} orders.`)
 
+takeOrder("extra cheese","Mushrooms")                       //2nd order
+console.log(`There are currently ${orderCount} orders.`)
+                                                            
+takeOrder("Beef","Pepperoni")                               //3rd order
+console.log(`There are currently ${orderCount} orders.`)
+
+takeOrder("Sausage","Tuna")                                 //4th order
+console.log(`There are currently ${orderCount} orders.`)    // total number of orders
