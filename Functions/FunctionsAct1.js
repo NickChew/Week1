2a354843d0b7077145db809b5b9d3e7e0a2503bf
NickChew <118730370+NickChew@users.noreply.github.com>
2022-11-30 12:13:32 +0000

    new updates

--- Functions/FunctionsAct1.js
+++ Functions/FunctionsAct1.js
@@ -2,6 +2,22 @@
 // Take this code and turn it into arrow function
 // syntax:
 
-function factorial (n) {
-    if ((n===0))
+// function factorial (n) {
+//     if ((n===0) || (n === 1)) {
+//         return 1;
+//     } else {
+//         return (n * factorial(n-1));
+//     }
+// }
+// console.log(factorial(33));
+
+// arrow function syntax of above
+
+const factorial = (n) => {
+    if ((n===0) || (n === 1)) {
+        return 1;
+    } else {
+        return (n * factorial(n-1));
+    }
 }
+console.log(factorial(33));
--- Functions/FunctionsAct3.js
+++ Functions/FunctionsAct3.js
@@ -1,5 +1,5 @@
-//Activity 3:
-//Cash machine time! JS
+//Activity 3:git add 
+//Cash machine time! 
 //Let’s create one that:
 //> Dispenses cash if your pin number is correct and your 
 //balance is equal to, or more than, the amount you’re 
